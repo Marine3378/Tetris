4746c701ad2e42560561250c1865c2556da69b70
Unknown <claire.delecrin@soprasteria.com>
2018-05-24 15:09:48 +0200

    Probleme???

--- tetris-model/src/main/java/tetris/model/DAOPartieJPA.java
+++ tetris-model/src/main/java/tetris/model/DAOPartieJPA.java
@@ -2,8 +2,19 @@ package tetris.model;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import tetris.model.HibernateUtils;
+
 public class DAOPartieJPA implements IDAOPartie {
 
+	private EntityManager em;
+	
+	public DAOPartieJPA() {
+		this.em= HibernateUtils.getEntityManager();
+	}
+	
+	@Override
 	public Partie save(Partie o) {
 		// TODO Auto-generated method stub
 		return null;
--- tetris-model/src/main/java/tetris/model/IDAO.java
+++ tetris-model/src/main/java/tetris/model/IDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IDAO <T> {
  public T save(T o);
  public boolean delete(T o);
- public T find(T o);
+ public T findById(T o);
  public List<T> findAll();
 
 }
